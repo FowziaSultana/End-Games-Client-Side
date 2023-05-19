2fec4a0144e727bdbf31ecd5d9bd86161270d297
FowziaSultana <fowziasultana168@gmail.com>
2023-05-19 13:34:29 +0600

    Added header section of home page

--- src/Pages/Home/HomeHeader/HomeHeader.jsx
+++ src/Pages/Home/HomeHeader/HomeHeader.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import kid1 from "../../../assets/image/kid4.jpg";
+import kid2 from "../../../assets/image/kid5.jpg";
+import "./HomeHeader.css";
 
 const HomeHeader = () => {
   return (
-    <div>
-      <p>I am home banner</p>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 place-items-center pt-8 px-5 bg-base-200 rounded-2xl">
+        <div className=" grid grid-col-1 lg:grid-cols-2  place-items-center">
+          <img
+            className="rounded-2xl max-h-[300px] lg:place-self-start"
+            src={kid1}
+          ></img>
+          <img className="hidden lg:block max-h-[300px]" src={kid2}></img>
+        </div>
+        <div className=" grid grid-cols-3 place-self-start my-10 mx-auto ">
+          <div className="col-span-3 lg:col-span-2 lg:max-w-[600px]   ">
+            <h1
+              className=" text-2xl font-bold lg:text-5xl text-[#fbbd23] 
+            "
+            >
+              Toys That Will Bring Delight to Your Children's Faces
+            </h1>
+          </div>
+          <div className="hidden lg:block  place-self-end ">
+            <h1
+              className=" text-2xl font-bold lg:text-2xl text-[#0b4027] bg-[#fbbd23] px-4 rounded
+            "
+            >
+              Happiness Always!!
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
