09ab3c39aa8b9540b993f6b068e6a10c55c9ff53
FowziaSultana <fowziasultana168@gmail.com>
2023-05-20 01:54:45 +0600

    updated all toys page

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const AllToys = () => {
+  const toys = useLoaderData();
+
   return (
     <div>
-      <p>ALl TOYS</p>
+      <p>ALl TOYS </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 items-center ">
+        {toys.map((aToy) => (
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img src={aToy.photo} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
